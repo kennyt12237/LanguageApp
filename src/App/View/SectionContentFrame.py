2d16dd1ebcdd56106e0fa25a2a24b3a790af2624
unknown <kennyt12237@gmail.com>
2024-08-04 12:36:43 +1200

    Changes frame when word button is pressed

--- src/App/View/SectionContentFrame.py
+++ src/App/View/SectionContentFrame.py
@@ -1,15 +1,22 @@
 from tkinter import Frame, Button
 from .WidgetUtils import packAllChildWidgets
+from .DictionaryFrame import DictionaryFrame
+from .Window import Window
 
 class SectionContentFrame(Frame):
     
-    def __init__(self, window, sectionContentData = None) -> None:
+    def __init__(self, window : Window, title : str, sectionData = None) -> None:
         super().__init__(window)
-        self.data = sectionContentData
-        self.wordButton = Button(self, text="Words")
+        self.window : Window = window
+        self.title : str = title
+        self.data : dict[str,list[dict[str,str]]] = sectionData
+        self.wordButton = Button(self, text="Words", command=lambda:self.onWordButtonPressed(self.data["words"]))
         self.grammarButton = Button(self, text="Grammars")
         self.sentenceButton = Button(self, text="Sentences")
         
+    def onWordButtonPressed(self, words : list[dict[str,str]]) -> None:
+        self.window.newFrameNavigated(DictionaryFrame(self.window,words))
+
     def pack(self) -> None:
         packAllChildWidgets(self)
         super().pack()
