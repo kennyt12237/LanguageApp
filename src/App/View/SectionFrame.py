be17eb37d1dfd2c9f1035c3a32e0628b85513220
unknown <kennyt12237@gmail.com>
2024-08-04 12:35:21 +1200

    Modified SectionFrame to create SectionContentFrame when a button is pressed, and calls the parent window to change frames

--- src/App/View/SectionFrame.py
+++ src/App/View/SectionFrame.py
@@ -1,17 +1,23 @@
 from tkinter import Frame, Button
 from .WidgetUtils import packAllChildWidgets, packForgetAllChildWidgets
+from .Window import Window
+from .SectionContentFrame import SectionContentFrame
 import json
 
 class SectionFrame(Frame):
     
-    def __init__(self, window, data : json = None) -> None:
+    def __init__(self, window : Window, data : json = None) -> None:
         super().__init__(window)
+        self.window = window
         self.sections = json.loads(data)
         self._createButtons()
         
+    def createSectionContentFrame(self, text, sectionData) -> None:
+        self.window.newFrameNavigated(SectionContentFrame(self.window, title=text, sectionData=sectionData))
+            
     def _createButtons(self) -> None:
         for key in self.sections:
-            Button(self, text=key)
+            Button(self, text=key, command=lambda key=key: self.createSectionContentFrame(text=key,sectionData = self.sections[key]))
     
     def pack(self) -> None:
         packAllChildWidgets(self)
