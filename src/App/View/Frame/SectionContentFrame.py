9623390456cde5ebf8465b4448200904ca4fbfa2
unknown <kennyt12237@gmail.com>
2024-08-07 00:43:04 +1200

    Switched SectionContentFrame to use grid

--- src/App/View/Frame/SectionContentFrame.py
+++ src/App/View/Frame/SectionContentFrame.py
@@ -18,6 +18,9 @@ class SectionContentFrame(Frame):
         self.wordButton = Button(self, text="Words", command=lambda:self.onWordButtonPressed(self.data["words"]))
         self.grammarButton = Button(self, text="Grammars", command=lambda:self.onGrammarButtonPressed(self.data["grammars"]))
         self.sentenceButton = Button(self, text="Sentences", command=lambda:self.onSentenceButtonPressed(self.data["sentences"]))
+        self.wordButton.grid(row=0, column=0)
+        self.grammarButton.grid(row=1, column=0)
+        self.sentenceButton.grid(row=2, column=0)
         
     def onWordButtonPressed(self, words : list[dict[str,str]]) -> None:
         self.window.newFrameNavigated(DictionaryFrame(self.window, words))
@@ -30,8 +33,3 @@ class SectionContentFrame(Frame):
         
     def getName(self) -> None:
         return self.name
-    
-    def pack(self) -> None:
-        packAllChildWidgets(self)
-        super().pack()
-        
