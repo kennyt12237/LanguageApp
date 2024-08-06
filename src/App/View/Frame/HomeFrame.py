d013ec4648f04f387cdc72eefe75ed65a6600c29
unknown <kennyt12237@gmail.com>
2024-08-07 00:01:17 +1200

    Added methods of setting home widget sizes and util method of text units

--- src/App/View/Frame/HomeFrame.py
+++ src/App/View/Frame/HomeFrame.py
@@ -4,18 +4,26 @@ from typing import Callable
 from .DictionaryFrame import DictionaryFrame
 from .SectionFrame import SectionFrame
 from ..Window import Window
-from ..Utils import packAllChildWidgets, packForgetAllChildWidgets
+from ..Utils import packAllChildWidgets, packForgetAllChildWidgets, convertPixelsToTextUnit
 
 class HomeFrame(Frame):
     
-    def __init__(self, window : Window, name : str = "Home"):
+    def __init__(self, window : Window, name : str = "Home", defaultWidgetSize : float = 0.1):
         super().__init__(window)
         self.window = window
         self.name = name
+        self.defaultWidgetSize = defaultWidgetSize
         self.getDictionaryData : Callable = None
         self.getSectionsData : Callable = None
         self.dictionaryButton = Button(self, text="Dictionary", command=self.onDictionaryButtonPressed)
-        self.sectionsButton = Button(self, text="Sections", command=self.onSectionsButtonPressed)
+        self.sectionButton = Button(self, text="Sections", command=self.onSectionsButtonPressed)
+        self.setWidgetSizeRelativeToScreen(defaultWidgetSize)
+        
+    def setWidgetSizeRelativeToScreen(self, relativeSize : float) -> None:
+        widthSize = int(self.window.getWidth() * relativeSize)
+        heightSize = int(self.window.getHeight() * relativeSize)
+        self.setDictionaryButtonSize(widthSize, heightSize)
+        self.setSectionButtonSize(widthSize, heightSize)
         
     def setGetDictionaryData(self, method : Callable) -> None:
         self.getDictionaryData = method
@@ -39,3 +47,11 @@ class HomeFrame(Frame):
     def pack_forget(self) -> None:
         packForgetAllChildWidgets(self)
         super().pack_forget()
+        
+    def setDictionaryButtonSize(self, widthPixels : int, heightPixels : int) -> None:
+        width, height = convertPixelsToTextUnit(self.dictionaryButton, widthPixels, heightPixels)
+        self.dictionaryButton.config(width=width, height=height)
+
+    def setSectionButtonSize(self, widthPixels : int, heightPixels : int) -> None:
+        width, height = convertPixelsToTextUnit(self.sectionButton, widthPixels, heightPixels)
+        self.sectionButton.config(width=width, height=height)
--- src/App/View/Utils/WidgetUtils.py
+++ src/App/View/Utils/WidgetUtils.py
@@ -1,4 +1,5 @@
-from tkinter import Widget
+from tkinter import Widget, font
+from math import floor
 
 def packAllChildWidgets(widget : Widget) -> None:
     for child in widget.winfo_children():
@@ -7,3 +8,13 @@ def packAllChildWidgets(widget : Widget) -> None:
 def packForgetAllChildWidgets(widget : Widget) -> None:
     for child in widget.winfo_children():
         child.pack_forget()
+    
+def convertPixelsToTextUnit(widget : Widget, widthPixel : int, heightPixel : int) -> tuple:
+    widgetFont = font.Font(font=widget.cget("font"))
+    charWidth = widgetFont.measure("0")
+    charHeight = widgetFont.metrics("linespace")
+    
+    widthInTextUnit = int(floor(widthPixel / charWidth))
+    heightInTextUnit = int(floor(heightPixel /  charHeight))
+    
+    return widthInTextUnit, heightInTextUnit
