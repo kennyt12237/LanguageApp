fa7ffe526680f608494baac6a1c2c5050a4d3d6e
unknown <kennyt12237@gmail.com>
2024-08-07 16:42:38 +1200

    Made GrammarFrame use GridFrame as superclass

--- src/App/View/Frame/GrammarFrame.py
+++ src/App/View/Frame/GrammarFrame.py
@@ -1,14 +1,13 @@
 from tkinter import Frame, Label
 import json
 
+from .GridFrame import GridFrame
 from ..Window import Window
 
-class GrammarFrame(Frame):
+class GrammarFrame(GridFrame):
     
     def __init__(self, window : Window, grammarData : list[dict[str,str]] = None, name : str = "Grammar") -> None:
-        super().__init__(window)
-        self.window : Window = window
-        self.name = name
+        super().__init__(window, name, width=window.getWidthMinusPadding(), height=int(window.getHeight() * 0.5), background="green")
         self.grammarData : list[dict[str,str]] = json.loads(grammarData)
         self._createLabels(self.grammarData)
         
@@ -22,6 +21,3 @@ class GrammarFrame(Frame):
                 labelText += str(value).strip()
             Label(self, text=labelText).grid(row=labelCount, column=0)
             labelCount = labelCount + 1
-
-    def getName(self) -> None:
-        return self.name
