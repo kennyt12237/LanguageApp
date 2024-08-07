a44692cf8c6d3393264736a7f10015f4358a758e
unknown <kennyt12237@gmail.com>
2024-08-07 16:09:25 +1200

    Added font, fontsize, padding, and positioning for NavigationFrame

--- src/App/View/Frame/NavigationFrame.py
+++ src/App/View/Frame/NavigationFrame.py
@@ -1,17 +1,20 @@
-from tkinter import Frame, Button, Label, LEFT, RIGHT
+from tkinter import Frame, Button, Label, W
 
 from ..Window import Window
 
 class NavigationFrame(Frame):
         
-    def __init__(self, window, name : str = "Navigation") -> None:
-        super().__init__(window)
+    def __init__(self, window : Window, name : str = "Navigation") -> None:
+        super().__init__(window, width=window.getWidthMinusPadding(), height=int(window.getHeight() * 0.2))
         self.window : Window = window
         self.name = name
-        self.backButton = Button(self, text="<-", command=self.backButtonPressed)
-        self.frameLabel = Label(self)
-        self.frameLabel.grid(row=0, column=1)
         self.window.bindForFrameChange(self.onFrameChanged)
+        self.backButton = Button(self, text="<-", command=self.backButtonPressed)
+        self.frameLabel = Label(self, font=("Arial", 18))
+        self.frameLabel.grid(row=0, column=1, pady=50, sticky=W)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.grid_propagate(False)
         
     def onFrameChanged(self, event) -> None:
         currentFrame = self.window.getCurrentFrame()
@@ -21,7 +24,7 @@ class NavigationFrame(Frame):
         if self.window.getNumberOfFramesNavigated() == 1:
             self.backButton.grid_forget()
             return
-        self.backButton.grid(row=0, column=0)
+        self.backButton.grid(row=0, column=0, sticky=W)
 
     def __setFrameHeader(self, frame : Frame) -> None:
         self.frameLabel.config(text=frame.getName())
