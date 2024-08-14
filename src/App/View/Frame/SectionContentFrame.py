1d3c7f6c9914b28f6736eaeee4832e0e829b40c6
unknown <kennyt12237@gmail.com>
2024-08-14 15:42:18 +1200

    Change button size for SectionContentFrame

--- src/App/View/Frame/SectionContentFrame.py
+++ src/App/View/Frame/SectionContentFrame.py
@@ -1,4 +1,5 @@
 from tkinter import Button, Misc
+from tkinter import CENTER
 
 from .DictionaryFrame import DictionaryFrame
 from .GrammarFrame import GrammarFrame
@@ -6,13 +7,15 @@ from .SentenceFrame import SentenceFrame
 from .GridFrame import GridFrame
 
 from ..Window import Window
+from ..Utils import convertPixelsToTextUnit
 
 class SectionContentFrame(GridFrame):
     
-    def __init__(self, master : Misc, sectionData = None, name : str = "section content", **kwargs) -> None:
+    def __init__(self, master : Misc, sectionData = None, defaultWidgetSize : float = 0.1, name : str = "section content", **kwargs) -> None:
         kwargs["name"] = name
         super().__init__(master, **kwargs)
         self.window : Window = self.winfo_toplevel()
+        self.defaultWidgetSize = defaultWidgetSize
         self.data : dict[str,list[dict[str,str]]] = sectionData
         self.wordButton = Button(self, text="Words", command=lambda:self.onWordButtonPressed(self.data["words"]))
         self.grammarButton = Button(self, text="Grammars", command=lambda:self.onGrammarButtonPressed(self.data["grammars"]))
@@ -20,6 +23,7 @@ class SectionContentFrame(GridFrame):
         self.wordButton.grid(row=0, column=0)
         self.grammarButton.grid(row=1, column=0)
         self.sentenceButton.grid(row=2, column=0)
+        self._setButtonProperties()
         
     def onWordButtonPressed(self, words : list[dict[str,str]]) -> None:
         self.window.newFrameNavigated(DictionaryFrame(self.window, words))
@@ -29,3 +33,14 @@ class SectionContentFrame(GridFrame):
         
     def onSentenceButtonPressed(self, sentences : list[dict[str,str]]) -> None:
         self.window.newFrameNavigated(SentenceFrame(self.window, sentences))
+        
+    def _setButtonProperties(self) -> None:
+        widthSize = int(self.window.getWidth() * self.defaultWidgetSize)
+        heightSize = int(self.window.getHeight() * self.defaultWidgetSize)
+        for widget in self.winfo_children():
+            if isinstance(widget, Button):
+                widthUnit, heightUnit = convertPixelsToTextUnit(widget, widthSize, heightSize)
+                widget.configure(width=widthUnit, height=heightUnit)
+            
+    def _setGridProperties(self) -> None:
+        self.grid_anchor(CENTER)
