a561b250a6b74d4cdde722ac5e32aa4b5bf1622a
unknown <kennyt12237@gmail.com>
2024-08-14 15:33:15 +1200

    Change button size for SectionFrame

--- src/App/View/Frame/SectionFrame.py
+++ src/App/View/Frame/SectionFrame.py
@@ -1,24 +1,43 @@
 from tkinter import Button, Misc
+from tkinter import CENTER
 import json
 
 from ..Window import Window
+from ..Utils import convertPixelsToTextUnit
 from .SectionContentFrame import SectionContentFrame
 from .GridFrame import GridFrame
 
 class SectionFrame(GridFrame):
     
-    def __init__(self, master : Misc, data : json = None, name : str = "section", **kwargs) -> None:
+    def __init__(self, master : Misc, data : json = None, defaultWidgetSize : float = 0.1, name : str = "section", **kwargs) -> None:
         kwargs["name"] = name
         super().__init__(master, **kwargs)
+        self.defaultWidgetSize = defaultWidgetSize
         self.window : Window = self.winfo_toplevel()
         self.sections = json.loads(data)
-        self._createButtons()
+        self.buttons : list[Button] = self._createButtons()
+        print("Buttons", self.buttons)
+        self._setButtonProperties()
         
     def createSectionContentFrame(self, text, sectionData) -> None:
         self.window.newFrameNavigated(SectionContentFrame(self.window, name=text, sectionData=sectionData))
             
-    def _createButtons(self) -> None:
+    def _createButtons(self) -> list[Button]:
+        buttonList : list[Button] = []
         buttonCount = 0
         for key in self.sections:
-            Button(self, text=key, command=lambda key=key: self.createSectionContentFrame(text=key,sectionData = self.sections[key])).grid(row=buttonCount, column=0)
+            button = Button(self, text=key, command=lambda key=key: self.createSectionContentFrame(text=key,sectionData = self.sections[key]))
+            button.grid(row=buttonCount, column=0)
+            buttonList.append(button)
             buttonCount = buttonCount + 1
+        return buttonList
+    
+    def _setButtonProperties(self) -> None:
+        widthSize = int(self.window.getWidth() * self.defaultWidgetSize)
+        heightSize = int(self.window.getHeight() * self.defaultWidgetSize)
+        for button in self.buttons:
+            widthUnit, heightUnit = convertPixelsToTextUnit(button, widthSize, heightSize)
+            button.config(width=widthUnit, height=heightUnit)
+            
+    def _setGridProperties(self) -> None:
+        self.grid_anchor(CENTER)
