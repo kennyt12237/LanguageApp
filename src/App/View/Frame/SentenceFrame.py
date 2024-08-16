276fdf7fce1840ab37bd48723f7ef5da6cbecca0
unknown <kennyt12237@gmail.com>
2024-08-16 14:09:20 +1200

    Added layout styling for SentenceFrame by setting grid properties

--- src/App/View/Frame/SentenceFrame.py
+++ src/App/View/Frame/SentenceFrame.py
@@ -1,9 +1,9 @@
 from tkinter import Frame, Label, Button, Misc
+from tkinter import CENTER, N, S, E, W
 from typing import Callable
 import json
 
 from .GridFrame import GridFrame
-from ..Window import Window
 
 class SentenceFrame(GridFrame):
     
@@ -12,10 +12,13 @@ class SentenceFrame(GridFrame):
         super().__init__(master, **kwargs)
         self.initIndex = initIndex
         self.sentenceData : list[dict[str,str]] = json.loads(sentenceData)
-        self.sentenceDataFrame = SentenceDataFrame(self, self._getSentenceDataIndexSentence(self.initIndex), self._getSentenceDataIndexMeaning(self.initIndex))
-        self.sentenceNavigationFrame = SentenceNavigationFrame(self, self.initIndex + 1, len(self.sentenceData), self.changeSentenceDataFrame)
-        self.sentenceDataFrame.grid(row=0, column=0)
-        self.sentenceNavigationFrame.grid(row=1, column=0)
+        self.sentenceDataFrame = SentenceDataFrame(self, self._getSentenceDataIndexSentence(self.initIndex), self._getSentenceDataIndexMeaning(self.initIndex), background="blue")
+        self.sentenceNavigationFrame = SentenceNavigationFrame(self, self.initIndex + 1, len(self.sentenceData), self.changeSentenceDataFrame, background="red")
+        self.sentenceDataFrame.grid(row=0, column=0, sticky=N+S+E+W)
+        self.sentenceNavigationFrame.grid(row=1, column=0, sticky=N+S+E+W)
+        self.grid_rowconfigure(0, weight=11)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
         
     def changeSentenceDataFrame(self, nextIndex : int) -> None:
         nextSentence = self._getSentenceDataIndexSentence(nextIndex)
@@ -31,10 +34,10 @@ class SentenceFrame(GridFrame):
     def _getSentenceDataIndexMeaning(self, index : int) -> str:
         return list(self.sentenceData[index].values())[1]
     
-class SentenceDataFrame(Frame):
+class SentenceDataFrame(GridFrame):
     
-    def __init__(self, rootFrame : Frame, sentence : str = None, meaning : str = None) -> None:
-        super().__init__(rootFrame)
+    def __init__(self, rootFrame : Frame, sentence : str = None, meaning : str = None, **kwargs) -> None:
+        super().__init__(rootFrame, **kwargs)
         self.rootFrame = rootFrame
         self.sentence = sentence
         self.meaning = meaning
@@ -47,10 +50,13 @@ class SentenceDataFrame(Frame):
         self.sentenceLabel.config(text=sentence)
         self.meaningLabel.config(text=meaning)
         
-class SentenceNavigationFrame(Frame):
+    def _setGridProperties(self) -> None:
+        self.grid_anchor(CENTER)
+        
+class SentenceNavigationFrame(GridFrame):
     
-    def __init__(self, rootFrame : Frame, initIndex : int = 1, totalSentences : int = 1, callback : Callable = None) -> None:
-        super().__init__(rootFrame)
+    def __init__(self, rootFrame : Frame, initIndex : int = 1, totalSentences : int = 1, callback : Callable = None, **kwargs) -> None:
+        super().__init__(rootFrame, **kwargs)
         self.rootFrame = rootFrame
         self.currentIndex = initIndex
         self.totalSentences = totalSentences
@@ -59,6 +65,10 @@ class SentenceNavigationFrame(Frame):
         self.stepLabel = Label(self, text=self._regenerateStepLabelText(self.currentIndex, totalSentences))
         self.nextButton = Button(self, text="Next", command=self._onNextButtonPressed)
         self.stepLabel.grid(row=0, column=1)
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 8)
+        self.grid_columnconfigure(2, weight = 1)
+        self.grid_rowconfigure(0, weight=1)
         self._determineButtonVisibility()
         
     def _regenerateStepLabelText(self, currentIndex : int = 1, totalSentence : int = 1) -> str:
@@ -85,16 +95,19 @@ class SentenceNavigationFrame(Frame):
             
     def __previousButtonVisibility(self) -> None:
         if self.currentIndex <= 1:
-            self.previousButton.grid_forget()
+            self.previousButton.grid_remove()
             return
-        self.previousButton.grid(row=0, column=0)
+        self.previousButton.grid(row=0, column=0, sticky=N+S+E+W)
         
     def __nextButtonVisibility(self) -> None:
         if self.currentIndex >= self.totalSentences:
-            self.nextButton.grid_forget()
+            self.nextButton.grid_remove()
             return
-        self.nextButton.grid(row=0, column=2)
+        self.nextButton.grid(row=0, column=2, sticky=N+S+E+W)
             
     def _determineButtonVisibility(self) -> None:
         self.__previousButtonVisibility()
         self.__nextButtonVisibility()
+        
+    def _setGridProperties(self) -> None:
+        self.grid_propagate(False)
