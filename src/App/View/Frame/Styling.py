e0735a5b4de8d8df5f0a79a3c0e7ada9ec8f53a5
unknown <kennyt12237@gmail.com>
2024-08-20 00:19:15 +1200

    Removed unused gridstyling

--- src/App/View/Frame/Styling.py
+++ src/App/View/Frame/Styling.py
@@ -4,21 +4,6 @@ WIDTH = "width"
 HEIGHT = "height"
 BACKGROUND = "background"
     
-class FrameStyling():
-    
-    def __init__(self, windowWidth : int = 0, windowHeight : int = 0, relativeSize : float = 0) -> None:
-        gridSetting = self.setGridSettings(windowWidth, windowHeight, relativeSize)
-    
-    def setGridSettings(self, windowWidth : int, windowHeight : int, relativeSize : float) -> None:
-        gridSettings : dict[str,str] = {}
-        gridSettings[WIDTH] = windowWidth
-        gridSettings[HEIGHT] = int(windowHeight * relativeSize)
-        gridSettings[BACKGROUND] = "green"
-        self.gridSetting = gridSettings
-
-    def getGridSettings(self) -> dict[str,str]:
-        return self.gridSetting
-    
 class TableStyling():
     
     def __init__(self) -> None:
@@ -30,6 +15,14 @@ class TableStyling():
     
     def getLabelSettings(self) -> dict:
         return self.labelSettings
+
+tableStyling = TableStyling()
+
+def getTableHeaderLabelSettings() -> dict:
+    return tableStyling.getHeaderLabelSettings()
+
+def getTableLabelSettings() -> dict:
+    return tableStyling.getLabelSettings()
     
 class SentenceStyling():
     
@@ -52,23 +45,6 @@ class SentenceStyling():
     
     def getStepLabelFont(self) -> tuple:
         return self.stepLabelFont
-    
-frameStyling = FrameStyling()
-
-def setGridSettings(windowWidth : int, windowHeight : int, relativeSize : float) -> dict[str,str]:
-    frameStyling.setGridSettings(windowWidth, windowHeight, relativeSize)
-    return frameStyling.getGridSettings()
-
-def getGridSettings() -> dict[str,str]:
-    return frameStyling.getGridSettings()
-
-tableStyling = TableStyling()
-
-def getTableHeaderLabelSettings() -> dict:
-    return tableStyling.getHeaderLabelSettings()
-
-def getTableLabelSettings() -> dict:
-    return tableStyling.getLabelSettings()
 
 sentenceStyling = SentenceStyling()
 
