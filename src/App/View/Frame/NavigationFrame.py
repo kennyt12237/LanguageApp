c48ad1a0d58406cb3d92957d45b72c142249f1cc
unknown <kennyt12237@gmail.com>
2024-08-20 00:22:08 +1200

    Made NavigationFrame a GridFrame

--- src/App/View/Frame/NavigationFrame.py
+++ src/App/View/Frame/NavigationFrame.py
@@ -1,24 +1,34 @@
 from tkinter import Frame, Button, Label, W
 
+from .AbstractFrame import GridFrame
 from ..Window import Window
 
-class NavigationFrame(Frame):
-        
-    def __init__(self, window : Window, name : str = "Navigation") -> None:
+class NavigationFrame(GridFrame):
+
+    BACK = '<-'
+    def __init__(self, window: Window) -> None:
         super().__init__(window)
-        self.window : Window = window
-        self.name = name
+        self.window: Window = window
         self.window.bindForFrameChange(self.onFrameChanged)
-        self.backButton = Button(self, text="<-", command=self.backButtonPressed)
+        self.backButton = Button(self, text=self.BACK,
+                                 command=self.backButtonPressed)
         self.frameLabel = Label(self, font=("Arial", 18))
-        self.frameLabel.grid(row=0, column=1, pady=50, sticky=W)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        self.grid_propagate(False)
+        self._gridPlacement()
+        self._setGridProperties()
         
+    def setDefaultFrame(self, defaultFrame: Frame) -> None:
+        self.__setFrameHeader(defaultFrame)
+
     def onFrameChanged(self, event) -> None:
         currentFrame = self.window.getCurrentFrame()
         self._newFrameNavigated(currentFrame)
+
+    def backButtonPressed(self) -> None:
+        self.window.returnToPreviousFrame()
+
+    def _newFrameNavigated(self, nextFrame: Frame) -> None:
+        self.__determineBackButtonChange()
+        self.__setFrameHeader(nextFrame)
     
     def __determineBackButtonChange(self) -> None:
         if self.window.getNumberOfFramesNavigated() == 1:
@@ -26,15 +36,14 @@ class NavigationFrame(Frame):
             return
         self.backButton.grid(row=0, column=0, sticky=W)
 
-    def __setFrameHeader(self, frame : Frame) -> None:
+    def __setFrameHeader(self, frame: Frame) -> None:
+        print(frame.winfo_name().capitalize())
         self.frameLabel.config(text=frame.winfo_name().capitalize())
-        
-    def setDefaultFrame(self, defaultFrame : Frame) -> None:
-        self.__setFrameHeader(defaultFrame)
-        
-    def _newFrameNavigated(self, nextFrame : Frame) -> None:
-        self.__determineBackButtonChange()
-        self.__setFrameHeader(nextFrame)
-       
-    def backButtonPressed(self) -> None:
-        self.window.returnToPreviousFrame()
+
+    def _gridPlacement(self) -> None:
+        self.frameLabel.grid(row=0, column=1, pady=50, sticky=W)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+    def _setGridProperties(self) -> None:
+        self.grid_propagate(False)
