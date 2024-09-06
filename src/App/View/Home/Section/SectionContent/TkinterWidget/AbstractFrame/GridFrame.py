1d7e1bcfe7c81fdea14e5786e44012bb96a08e22
unknown <kennyt12237@gmail.com>
2024-09-06 20:46:46 +1200

    Fixed an issue when another instance of Tk was created when using a created Widget in View

--- src/App/View/Home/Section/SectionContent/TkinterWidget/AbstractFrame/GridFrame.py
+++ src/App/View/Home/Section/SectionContent/TkinterWidget/AbstractFrame/GridFrame.py
@@ -5,7 +5,7 @@ from tkinter import CENTER
 class GridFrame(Frame):
 
     def __init__(self, master: Misc, **kwargs) -> None:
-        super().__init__(master, kwargs)
+        super().__init__(master, **kwargs)
         self.row = 0
         self.column = 0
         self._setGridProperties()
