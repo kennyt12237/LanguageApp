2ea5aab81a03dfe0915bd49a62c019bf09bf2a1f
unknown <kennyt12237@gmail.com>
2024-09-12 17:54:41 +1200

    Changed Grammar number to Grammar version

--- src/App/View/Home/Section/SectionContent/TkinterWidget/SentenceFrame/SentenceDataFrame.py
+++ src/App/View/Home/Section/SectionContent/TkinterWidget/SentenceFrame/SentenceDataFrame.py
@@ -195,7 +195,7 @@ class GrammarLabelTooltip(LabelTooltip):
         text = "Pinyin : {pinyin}\nCharacter : {character}{num}\nMeaning : {meaning}".format(
             pinyin=data["pinyin"],
             character=data["character"],
-            num=data["number"],
+            num=data["version"],
             meaning=data["usage"])
         self.toolTip.configure(text=text)
 
