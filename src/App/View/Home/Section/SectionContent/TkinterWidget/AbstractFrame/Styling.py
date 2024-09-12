045aa85aa904893bc6541d6898f82a7befbebdfa
unknown <kennyt12237@gmail.com>
2024-09-12 17:48:26 +1200

    Fixed an issue where the length of grammar meaning took out the entire row length. Also changed the styling of GridFrame to the new version instead of prior calling object methods.

--- src/App/View/Home/Section/SectionContent/TkinterWidget/AbstractFrame/Styling.py
+++ src/App/View/Home/Section/SectionContent/TkinterWidget/AbstractFrame/Styling.py
@@ -3,27 +3,6 @@ WIDTH = "width"
 HEIGHT = "height"
 BACKGROUND = "background"
 
-
-class TableStyling():
-
-    def __init__(self) -> None:
-        self.headerLabelSettings = dict(
-            borderwidth=2, relief="groove", font=("Segoe UI", 16))
-        self.labelSettings = dict(borderwidth=2, relief="groove")
-
-    def getHeaderLabelSettings(self) -> dict:
-        return self.headerLabelSettings
-
-    def getLabelSettings(self) -> dict:
-        return self.labelSettings
-
-
-tableStyling = TableStyling()
-
-
-def getTableHeaderLabelSettings() -> dict:
-    return tableStyling.getHeaderLabelSettings()
-
-
-def getTableLabelSettings() -> dict:
-    return tableStyling.getLabelSettings()
+defaultHeaderLabelSettings = dict(
+    borderwidth=2, relief="groove", font=("Segoe UI", 16))
+defaultLabelSettings =dict(borderwidth=2, relief="groove", wraplength=400)
