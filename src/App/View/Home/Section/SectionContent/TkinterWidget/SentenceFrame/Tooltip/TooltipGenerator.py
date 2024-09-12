c602386629778b13fa304ed91d3eb831a06a7891
unknown <kennyt12237@gmail.com>
2024-09-12 18:06:21 +1200

    Fixed an issue when createToooltipLabel was sing default tooltip name

--- src/App/View/Home/Section/SectionContent/TkinterWidget/SentenceFrame/Tooltip/TooltipGenerator.py
+++ src/App/View/Home/Section/SectionContent/TkinterWidget/SentenceFrame/Tooltip/TooltipGenerator.py
@@ -10,16 +10,19 @@ class TooltipGenerator():
         self.labelName = labelName
         self.number = startingNumber
 
-    def createTooltipLabel(self, master: Misc, tooltipParent: Misc, **kwargs) -> TooltipLabel:
-        tooltipName = self.DEFAULT_TOOLTIP_TEXT + str(self.__getNumber())
-        self.__incrementNumber()
-        return self._createTooltipLabel(master, tooltipParent, tooltipName=tooltipName, **kwargs)
-
     def __incrementNumber(self) -> None:
         self.number += 1
 
+    def getLabelName(self) -> str:
+        return self.labelName
+    
     def __getNumber(self) -> int:
         return self.number
+
+    def createTooltipLabel(self, master: Misc, tooltipParent: Misc, **kwargs) -> TooltipLabel:
+        tooltipName = self.getLabelName() + str(self.__getNumber())
+        self.__incrementNumber()
+        return self._createTooltipLabel(master, tooltipParent, tooltipName=tooltipName, **kwargs)
 
     def _createTooltipLabel(self, master: Misc, tooltipParent: Misc, tooltipName: str, **kwargs):
         return TooltipLabel(master, tooltipParent, tooltipName=tooltipName, **kwargs)
